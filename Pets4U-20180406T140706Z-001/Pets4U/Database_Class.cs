446c5408ab57b7622c990aa3aa9682e1bbd17726
Deon_P <Gideondeonjacobus@Gmail.com>
2018-04-21 08:30:16 +0200

    Added to database class staff registgration

--- Pets4U-20180406T140706Z-001/Pets4U/Database_Class.cs
+++ Pets4U-20180406T140706Z-001/Pets4U/Database_Class.cs
@@ -20,15 +20,53 @@ namespace Pets4U
 
         
 
-        public void insert_staff()
+        public void insert_staff(int Staff_Number, string Staff_Lname, string Staff_Fname, string Staff_Street, string Staff_City,
+                                 string Staff_State, int Staff_Zip, int Staff_Tel, string Staff_Birth_Date, string Staff_Sex,
+                                 int Staff_ID, string Staff_Position, double Staff_Annual_Salary, int Clinic_Number)
         {
-            MySqlParameter[] param = new MySqlParameter[2];
+            MySqlParameter[] param = new MySqlParameter[14];
 
-            param[0] = new MySqlParameter("Lname", MySqlDbType.VarChar);
-            param[0].Value = "Deon";
+            param[0] = new MySqlParameter("Staff_Number", MySqlDbType.Int32);
+            param[0].Value = Staff_Number;
 
-            param[1] = new MySqlParameter("Fname", MySqlDbType.VarChar);
-            param[1].Value = "Pieterse";
+            param[1] = new MySqlParameter("Staff_Lname", MySqlDbType.VarChar);
+            param[1].Value = Staff_Lname;
+
+            param[2] = new MySqlParameter("Staff_Fname", MySqlDbType.VarChar);
+            param[2].Value = Staff_Fname;
+
+            param[3] = new MySqlParameter("Staff_Street", MySqlDbType.VarChar);
+            param[3].Value = Staff_Street;
+
+            param[4] = new MySqlParameter("Staff_City", MySqlDbType.VarChar);
+            param[4].Value = Staff_City;
+
+            param[5] = new MySqlParameter("Staff_State", MySqlDbType.VarChar);
+            param[5].Value = Staff_State;
+
+            param[6] = new MySqlParameter("Staff_Zip", MySqlDbType.Int32);
+            param[6].Value = Staff_Zip;
+
+            param[7] = new MySqlParameter("Staff_Tel", MySqlDbType.Int32);
+            param[7].Value = Staff_Tel;
+
+            param[8] = new MySqlParameter("Staff_Birth_Date", MySqlDbType.Date);
+            param[8].Value = Staff_Birth_Date;
+
+            param[9] = new MySqlParameter("Staff_Sex", MySqlDbType.VarChar);
+            param[9].Value = Staff_Sex;
+
+            param[10] = new MySqlParameter("Staff_ID", MySqlDbType.Int32);
+            param[10].Value = Staff_ID;
+
+            param[11] = new MySqlParameter("Staff_Position", MySqlDbType.VarChar);
+            param[11].Value = Staff_Position;
+
+            param[12] = new MySqlParameter("Staff_Annual_Salary", MySqlDbType.Double);
+            param[12].Value = Staff_Annual_Salary;
+
+            param[13] = new MySqlParameter("Clinic_Number", MySqlDbType.Int32);
+            param[13].Value = Clinic_Number;
 
             MySqlCommand command = new MySqlCommand();
 
@@ -41,11 +79,11 @@ namespace Pets4U
             connection.Open();
             if (command.ExecuteNonQuery() == 1)
             {
-                MessageBox.Show("Query executed.");
+                MessageBox.Show("Staff member has been registered.", "Registered Staff Member" , MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
             {
-                MessageBox.Show("Query not executed.");
+                MessageBox.Show("Staff member has not been registered.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
             connection.Close();
         }
@@ -86,11 +124,11 @@ namespace Pets4U
             connection.Open();
             if (command.ExecuteNonQuery() == 1)
             {
-                MessageBox.Show("Query executed.");
+                MessageBox.Show("Clinic has been registered.", "Registered Clinic", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
             {
-                MessageBox.Show("Query not executed.");
+                MessageBox.Show("Clinic has not been registered.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             }
             connection.Close();
         }
